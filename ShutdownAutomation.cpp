2327ece43238d95de9a0075fdbdc54182813544a
Xiaoxuan4096 <Xiaoxuan-CodeSubmitter@outlook.com>
2026-04-30 22:41:53 +0800

    写注释。

--- ShutdownAutomation.cpp
+++ ShutdownAutomation.cpp
@@ -1,13 +1,21 @@
-﻿#include <iostream>
+﻿// ShutdownAutomation - Simple shutdown program on Windows.
+// Written by Xiaoxuan4096.
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
+
+#include <iostream>
 #include <cstdlib>
+
 int main(int argc, char* argv[]) {
     if (argc == 1) {
         std::cout << "No argument supplied, canceling shutdown by default." << std::endl;
         system("shutdown -a");
     }
+
     if (argc == 2) {
-        std::cout << argv[1] << std::endl;
-        if (argv[1] == "-shutdown") {
+        std::cout << argv[1] << std::endl; // For debug purpose only. Need to be removed.
+        if (argv[1] == "shutdown") {
             std::cout << "Received shutdown argument, proceeding shutdown." << std::endl;
             system("shutdown -s -t 60");
         }
