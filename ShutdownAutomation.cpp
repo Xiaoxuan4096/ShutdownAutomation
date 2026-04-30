862eebae3deb6b956930096ea9e0a3210a6abbb7
Xiaoxuan4096 <Xiaoxuan-CodeSubmitter@outlook.com>
2026-04-30 22:37:56 +0800

    添加项目文件。

---
+++ ShutdownAutomation.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cout << "No argument supplied, canceling shutdown by default." << std::endl;
+        system("shutdown -a");
+    }
+    if (argc == 2) {
+        std::cout << argv[1] << std::endl;
+        if (argv[1] == "-shutdown") {
+            std::cout << "Received shutdown argument, proceeding shutdown." << std::endl;
+            system("shutdown -s -t 60");
+        }
+        else {
+            std::cout << "Not received shutdown argument, canceling shutdown by default." << std::endl;
+            system("shutdown -a");
+        }
+    }
+    if (argc > 2) {
+        std::cout << "Too many arguments supplied, canceling shutdown by default." << std::endl;
+        system("shutdown -a");
+    }
+    return 0;
+}
